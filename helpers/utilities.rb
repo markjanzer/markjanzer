e38de1dcdf3ca0430688e70f212e401972c8b224
markjanzer <mark.janzer@gmail.com>
2017-12-16 16:29:27 -0800

    Remove a lot of old code. Add the a lot of content for my own site.

--- helpers/utilities.rb
+++ helpers/utilities.rb
@@ -1,33 +1,33 @@
 module Utilities
   
   # Active link_to
-  def active_link_to(*args, &block)
+  # def active_link_to(*args, &block)
 
-    # Check for block
-    url_idx = block_given? ? 0 : 1
-    options_idx = block_given? ? 1 : 2
+  #   # Check for block
+  #   url_idx = block_given? ? 0 : 1
+  #   options_idx = block_given? ? 1 : 2
 
-    # Get the url
-    page = current_page.url
+  #   # Get the url
+  #   page = current_page.url
 
-    # Set the url
-    url = args[url_idx]
+  #   # Set the url
+  #   url = args[url_idx]
 
-    # Get the parent url
-    unless page == "/" || page == ""
-      parent_url = "/" + page.to_s.split("/")[1] + "/"
-    end
+  #   # Get the parent url
+  #   unless page == "/" || page == ""
+  #     parent_url = "/" + page.to_s.split("/")[1] + "/"
+  #   end
 
-    options = args[options_idx] || {}
-    options[:class] ||= ""
+  #   options = args[options_idx] || {}
+  #   options[:class] ||= ""
 
-    if (url == page || url == parent_url)
-      options[:class] << " is-active"
-      args[options_idx] = options
-    end
+  #   if (url == page || url == parent_url)
+  #     options[:class] << " is-active"
+  #     args[options_idx] = options
+  #   end
     
-    link_to(*args, &block)
-  end
+  #   link_to(*args, &block)
+  # end
 
   def image_srcset(image_src, mobile_2x)
     srcset = ""
--- source/assets/javascripts/application.js
+++ source/assets/javascripts/application.js
@@ -1,5 +1,6 @@
 //= require vendor/_fastclick.min
 //= require vendor/_picturefill.min
+//= require vendor/_tessarray.min
 //= require blocks/_menu
 
 if ('addEventListener' in document) {
@@ -8,5 +9,122 @@ if ('addEventListener' in document) {
   }, false);
 }
 
+// How does this work without parallax?
 // var scene = document.getElementById('scene');
 // var parallaxInstance = new Parallax(scene);
+
+var tessarrayExample = new Tessarray("#container", ".box", {
+  containerTransition: {
+    duration: 500,
+    timingFunction: "cubic-bezier(0.320, 1.385, 0.730, -0.470)",
+    delay: 0
+  }
+});
+
+const bindListenerToDocument = (event, elementClass, callback) => {
+  if (Array.isArray(elementClass)) {
+    document.addEventListener(event, (event) => {
+      if (elementClass.some((element) => event.target.classList.contains(element))) {
+        callback(event);
+      }
+    });
+  } else {
+    document.addEventListener(event, (event) => {
+      if (event.target.classList.contains(elementClass)) {
+        callback(event);
+      }
+    });
+  }
+}
+
+const $ = (selector) => {
+  if (selector.charAt(0) === "#") {
+    return document.querySelector(selector);
+  } else {
+    return document.querySelectorAll(selector);
+  }
+}
+
+bindListenerToDocument("click", "project-title", (event) => openProject(event));
+document.addEventListener("click", event => {
+  if ($(".project.is-active").length > 0) {
+    if (!event.target.closest(".project-body")) {
+      closeAllProjects();
+      console.log("closeAllProjects");
+    }
+  }
+})
+
+function openProject(event) {
+  console.log("openProject");
+  closeAllProjects();
+  var projectTitle = event.target;
+  var project = projectTitle.closest(".project");
+  var projectBackground = project.querySelector(".project-background");
+  projectBackground.style.zIndex = "2";
+  project.classList.add("is-active");
+  project.classList.remove("shake-trigger");
+  tessarrayExample.render();
+  // open();
+}
+
+function closeAllProjects(event) {
+  var openProjects = $(".project.is-active");
+  Array.from(openProjects).forEach(project => {
+    project.classList.remove("is-active");
+    project.classList.add("shake-trigger");
+    setTimeout(() => {
+      var projectBackground = project.querySelector(".project-background");
+      projectBackground.style.zIndex = "-1";
+    }, 1000)
+    // Array.from(project.querySelectorAll(".hide")).forEach(hideElement => {
+    //   hideElement.addEventListener("webkitTransitionEnd", positionAbsolute);
+    // });
+  });
+}
+
+function positionAbsolute() {
+  console.log("positionAbsolute");
+  console.log(this);
+  this.style.position = "absolute";
+  this.removeEventListener("webkitTransitionEnd", positionAbsolute)
+}
+
+bindListenerToDocument("click", "job-title", (event) => openJob(event));
+document.addEventListener("click", event => {
+  if ($(".job.is-active").length > 0) {
+    if (!event.target.closest(".job-body")) {
+      closeAllJobs();
+    }
+  }
+});
+
+function openJob(event) {
+  console.log("openJob");
+  closeAllJobs();
+  var jobTitle = event.target;
+  var job = jobTitle.closest(".job");
+  var jobBackground = job.querySelector(".job-background");
+  jobBackground.style.zIndex = "2";
+  job.classList.add("is-active");
+  job.classList.remove("shake-trigger");
+}
+function closeAllJobs(event) {
+  var openJobs = $(".job.is-active");
+  Array.from(openJobs).forEach(job => {
+    job.classList.remove("is-active");
+    job.classList.add("shake-trigger");
+    setTimeout(() => {
+      var jobBackground = job.querySelector(".job-background");
+      jobBackground.style.zIndex = "-1";
+    }, 1000)
+  });
+}
+
+// Letter Tessarray
+// function open() {
+//   Array.from($(".hide")).forEach(hideElement => {
+//     hideElement.style.position = "relative";
+//   });
+// }
+
