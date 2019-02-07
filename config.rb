e47624bbab38b92368066b06012728d4a84e9e00
markjanzer <mark.janzer@gmail.com>
2019-02-06 16:04:48 -0800

    Remove vestigal code.

--- config.rb
+++ config.rb
@@ -3,19 +3,10 @@ require "sprockets/es6"
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 # Global Configuration
 
-# Global variables
-set :base_url,  "https://dixonandmoe.com"
-set :base_name, "Dixon & Moe"
-set :base_email, "hello@dixonandmoe.com"
-set :base_imgix, "https://dixonandmoe.imgix.net"
-set :base_domain, "dixonandmoe.com"
-set :base_twitter, "@dixonandmoe"
-
 # Pages with no layout
 page "/*.xml",  layout: false
 page "/*.json", layout: false
 page "/*.txt",  layout: false
-page "/rellax.html", layout: false
 
 # Asset paths
 set :css_dir,    "assets/stylesheets"
--- source/assets/javascripts/application.js
+++ source/assets/javascripts/application.js
@@ -1,7 +1,5 @@
 //= require vendor/_fastclick.min
-//= require vendor/_picturefill.min
 //= require vendor/_tessarray.min
-//= require blocks/_menu
 
 if ('addEventListener' in document) {
   document.addEventListener('DOMContentLoaded', function() {
